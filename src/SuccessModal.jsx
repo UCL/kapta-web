686416d0391626c9386646e5a782d250f1534e66
acholyn <a.ho-lyn@ucl.ac.uk>
2024-10-23 13:10:35 +0100

    making success modal more flexible

--- src/SuccessModal.jsx
+++ src/SuccessModal.jsx
@@ -1,26 +1,36 @@
 import { Button } from "@mui/material";
-import "./styles/task-form.css";
+import "./styles/dialogs.css";
 
 export default function SuccessModal({
-	taskID,
-	title,
-	description,
-	campaignCode,
+	taskID = "",
+	taskTitle = "",
+	taskDescription = "",
+	campaignCode = "",
 	setSuccessModalVisible,
+	isTask,
 }) {
+	// if it's a not a task, the title will come through as taskTitle
+	let title;
+	if (isTask) title = "Your submission has been successful. See details below";
+	else title = taskTitle;
 	return (
 		<dialog open id="success-dialog">
-			<h3>Your submission has been successful. See details below</h3>
-			<p>Request ID: {taskID}</p>
-			<p>Task title:{title}</p>
-			<p>Task description:{description}</p>
-			<div>
-				<p>
-					Below is the campaign code for people to access your study on Kapta
-					Mobile and contribute to it.
-				</p>
-				<div className="campaign-code">{campaignCode}</div>
-			</div>
+			<h3>{title}</h3>
+			{isTask && (
+				<>
+					{" "}
+					<p>Request ID: {taskID}</p>
+					<p>Task title:{taskTitle}</p>
+					<p>Task description:{taskDescription}</p>
+					<div>
+						<p>
+							Below is the campaign code for people to access your study on
+							Kapta Mobile and contribute to it.
+						</p>
+						<div className="campaign-code">{campaignCode}</div>
+					</div>
+				</>
+			)}
 			<Button variant="contained" onClick={() => setSuccessModalVisible(false)}>
 				Close
 			</Button>
