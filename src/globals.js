90fb2a32b372adbc53af669bafd8427114b63eaf
acholyn <a.ho-lyn@ucl.ac.uk>
2024-11-18 12:05:25 +0000

    removing some vars (unused)

--- src/globals.js
+++ src/globals.js
@@ -3,23 +3,8 @@ import { UserContext } from "./utils/UserContext";
 
 // export vars used throughout the app
 export const REQUEST_URL = import.meta.env.VITE_REQUEST_URL;
-export const WEB_POOL_ID = import.meta.env.VITE_WEB_POOL_ID;
-export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-export const REGION = import.meta.env.VITE_REGION;
 export const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 export const WA_CHAT_URL = import.meta.env.VITE_WA_CHAT_URL;
-
-export let cognito = null;
-export let hasCognito = false;
-if (WEB_POOL_ID !== "null" && CLIENT_ID !== "null") {
-	// checking for null string due to amplify store
-	hasCognito = true;
-	cognito = {
-		userPoolId: WEB_POOL_ID,
-		userPoolClientId: CLIENT_ID,
-		region: REGION,
-	};
-}
 
 export const useUserStore = () => {
 	return useContext(UserContext);
